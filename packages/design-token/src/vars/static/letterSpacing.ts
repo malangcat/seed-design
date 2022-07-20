e0120ff602ae9eace088ad7b9ffc2d6c49bc7b37
Asher <102206520+malangcat@users.noreply.github.com>
2022-07-20 09:52:16 +0900

    feat: Update color tokens

---
+++ packages/design-token/src/vars/static/letterSpacing.ts
@@ -0,0 +1,4 @@
+export const none = 'var(--seed-static-letter-spacing-none)';
+export const small = 'var(--seed-static-letter-spacing-small)';
+export const han = 'var(--seed-static-letter-spacing-han)';
+export const medium = 'var(--seed-static-letter-spacing-medium)';
