f85c38142b7ed3e4a8c48027e3a5b734c0d97a70
Asher <102206520+malangcat@users.noreply.github.com>
2022-07-22 18:36:57 +0900

    feat: Update color tokens

---
+++ packages/design-token/src/vars/static/lineHeight.ts
@@ -0,0 +1,4 @@
+export const static100 = 'var(--seed-static-line-height-static-100)';
+export const static130 = 'var(--seed-static-line-height-static-130)';
+export const static150 = 'var(--seed-static-line-height-static-150)';
+export const static170 = 'var(--seed-static-line-height-static-170)';
