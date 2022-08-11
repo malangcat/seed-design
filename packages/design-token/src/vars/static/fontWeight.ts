a73358bfe5ec5cf90dc8472873c6b31274080786
Asher <102206520+malangcat@users.noreply.github.com>
2022-08-11 11:51:54 +0900

    feat: Update color tokens

---
+++ packages/design-token/src/vars/static/fontWeight.ts
@@ -0,0 +1,3 @@
+export const static100 = 'var(--seed-static-font-weight-static-100)';
+export const static400 = 'var(--seed-static-font-weight-static-400)';
+export const static700 = 'var(--seed-static-font-weight-static-700)';
